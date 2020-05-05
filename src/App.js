ff9a77b0f3d3fe42e5e515183de7c7d2a12bb4ab
Erik Ford <ekford18@gmail.com>
2020-05-04 17:36:33 -0700

    corrected pagination

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ export default class App extends Component {
     render() {
         return (
             <div>
+                <div>
+                    Header
+                </div>
                 <Router>
                     <Switch>
                         <Route 
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -8,35 +8,36 @@ export default class SearchPage extends Component {
 
 
 state = {
+  selectedFilter: 'pokemon',
   search: '',
   dropdown: 'pokemon',
   data: [],
   page: 1,
-  info: {}
+  info: {},
+  hidePrevious: true
+  
 }
-
-// async componentDidMount() {
-//   const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex`)
-//   this.setState({ data: data.body.results })
-// }
 
 async componentDidMount() {
   const searchParams = new URLSearchParams(window.location.search)
   const search = searchParams.get('search');
-  const filter = searchParams.get('dropdown')
 
-  this.setState( {search, dropdown: filter});
+  const filter = searchParams.get('dropdown')
 
   if (search){
     let page = 1;
     if (searchParams.get('page')){
       page = searchParams.get('page');
     }
-    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?${filter || this.state.dropdown}=${search}&page=${page}`)
+    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?${filter}=${search}&page=${page}`)
     const data = fetchedData.body.results;
     const info = fetchedData.body.info;
     this.setState({ data: data, info: info })
   }
+    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex`)
+    const data = fetchedData.body.results;
+    const info = fetchedData.body.info;
+    this.setState({ data: data, info: info })
 }
   
 handleDropDownChange = (e) => {
@@ -51,8 +52,9 @@ handleDropDownChange = (e) => {
   }
 
   handleClick = async () => {
-    this.setState({ loading: true })
-    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?${this.state.dropdown}=${this.state.search}&page=${this.state.page}`);
+    this.setState({ loading: true });
+    const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?${this.state.dropdown}=${this.state.search}&page=${this.state.page}`)
+     
     const info = fetchedData.body.info;
     this.setState({ loading: false })
     this.setState({ data: fetchedData.body.results, info: info })
@@ -61,11 +63,15 @@ handleDropDownChange = (e) => {
     routeToNextPage = async () => {
       const nextPageNumber = this.state.page + 1;
       this.setState({ page: nextPageNumber })
-      console.log(this.state.page)
       const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?${this.state.dropdown}=${this.state.search}&page=${nextPageNumber}`)
-      console.log(fetchedData)
       const data = fetchedData.body.results;
-      const info = fetchedData.body.info;
+      const info = fetchedData.body;
+      this.setState({hidePrevious: false})
+      if(this.state.page === (Math.ceil(info.count/info.perPage))) {
+        this.setState({hideNext: true})
+      } else {
+        this.setState({hideNext: false})
+      }
       this.setState({ data: data, info: info, page: nextPageNumber})
     }
 
@@ -75,7 +81,13 @@ handleDropDownChange = (e) => {
 
       const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?${this.state.dropdown}=${this.state.search}&page=${previousPageNumber}`)
       const data = fetchedData.body.results;
-      const info = fetchedData.body.info;
+      const info = fetchedData.body;
+      this.setState({hideNext: false})
+      if(this.state.page === 1) {
+        this.setState({hidePrevious: true})
+      } else {
+        this.setState({hidePrevious: false})
+      }
       this.setState({ data: data, info: info })
     }
 
@@ -83,9 +95,9 @@ handleDropDownChange = (e) => {
     return (
       <body> 
         <header>
-          {this.state.page && <button onClick={this.routeToPreviousPage}>Previous</button>}
+          {!this.state.hidePrevious && <button onClick={this.routeToPreviousPage}>Previous</button>}
           Pokedex
-          {this.state.page && <button onClick={this.routeToNextPage}>Next</button>}
+          {!this.state.hideNext && <button onClick={this.routeToNextPage}>Next</button>}
         </header>    <main>
               <SearchSection selectedFilter={this.state.dropdown}
             CallBackhandleChange={this.handleChange}
@@ -100,7 +112,4 @@ handleDropDownChange = (e) => {
       </body>
     )
   }
-}  
-         
-        
-
+}
